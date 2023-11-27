2986ddffb7db720c4c047911ce32dc41010ee2bc
NickJCoder <151190011+NickJCoder@users.noreply.github.com>
2023-11-27 19:19:07 +0000

    excusecreator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+const action = ['ate', 'peed', 'crushed', 'broke'];
+const what = ['my homework', 'the keys', 'the car'];
+const when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateExcuse() {
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+
+  document.getElementById('excuse').innerText = excuse;
+}
